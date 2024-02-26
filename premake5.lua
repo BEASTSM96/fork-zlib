fc93b9f5ff79fe499c8704ed78a7565db2dd2e21
BEASTSM96 <beastboioioi.dev@gmail.com>
2024-02-26 17:58:11 +0000

    Add Premake file
    - Remove files that won't be needed

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "Tracy"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.c"
+	}
+
+	defines 
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	links
+	{
+		"ws2_32",
+		"dbghelp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
